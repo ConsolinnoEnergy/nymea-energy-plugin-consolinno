c63b08d1e322f5cec515dc2dedb0baa5f8b4144d
Tim Fischer <t.fischer@consolinno.de>
2024-06-06 13:14:04 +0200

    disable HP first

--- energyengine.cpp
+++ energyengine.cpp
@@ -1019,9 +1019,9 @@ void EnergyEngine::evaluateAndSetMaxChargingCurrent()
     double minPhaseMarginPower
         = 230 * m_housholdPhaseLimit; // the minPhaseMarginPower is the minimum available power per
                                       // phase for which it can be increased
-    double maxAllowedChargingCurrentPhase = 0;
-    double minAllowedChargingCurrentPhase = 0;
-    double actualChargingCurrentLimitPhase = 0;
+    double maxChargingCurrentPerPhaseMaxValue = 0;
+    double maxChargingCurrentPerPhaseMinValue = 0;
+    double maxChargingCurrentPerPhase = 0;
 
     // Check if the power consumption limit is exceeded in regards to phasePowerLimit
     qCDebug(dcConsolinnoEnergy()) << "Houshold physical phase limit:" << m_housholdPhaseLimit
@@ -1029,7 +1029,41 @@ void EnergyEngine::evaluateAndSetMaxChargingCurrent()
     qCDebug(dcConsolinnoEnergy()) << "Houshold physical power limit total:" << phasePowerLimit * 3
                                   << "[W] at 230V";
 
-    // first check if any of the currents exceed the limit
+    /*
+    Adding the logic for the heat pumps
+    */
+    foreach (Thing* thing, m_heatPumps) {
+        if (m_consumptionLimit > 0) {
+
+            qCDebug(dcConsolinnoEnergy())
+                << "Blackout protection: Turning off heat pump with name: " << thing->name()
+                << " and id: " << thing->id();
+
+            Action action(ActionTypeId("82b38d32-a277-41bb-a09a-44d6d503fc7a"), thing->id());
+            ParamList params;
+            params.append(Param(ParamTypeId("82b38d32-a277-41bb-a09a-44d6d503fc7a"), "Off"));
+            action.setParams(params);
+            m_thingManager->executeAction(action);
+
+            qCInfo(dcConsolinnoEnergy()) << "Blackout protection: Heat pump turned off.";
+        } else {
+            qCDebug(dcConsolinnoEnergy())
+                << "Blackout protection: Turning on heat pump with name: " << thing->name()
+                << " and id: " << thing->id();
+
+            Action action(ActionTypeId("82b38d32-a277-41bb-a09a-44d6d503fc7a"), thing->id());
+            ParamList params;
+            params.append(Param(ParamTypeId("82b38d32-a277-41bb-a09a-44d6d503fc7a"), "Standard"));
+            action.setParams(params);
+            m_thingManager->executeAction(action);
+
+            qCInfo(dcConsolinnoEnergy()) << "Blackout protection: Heat pump turned on.";
+        }
+    }
+
+    /*
+    first check if any of the currents exceed the limit
+    */
     foreach (const QString& phase, allPhasesCurrentPower.keys()) {
         if (allPhasesCurrentPower.value(phase) > phasePowerLimit) {
             double phaseOvershotPower = allPhasesCurrentPower.value(phase) - phasePowerLimit;
@@ -1128,31 +1162,30 @@ void EnergyEngine::evaluateAndSetMaxChargingCurrent()
             << "Consumption limit is not set because m_consumptionLimit is: " << m_consumptionLimit;
     }
 
-    // if the limit is exceeded or below max, we adjust the charging current for each EV charger
+    /*
+    ev chargers
+    */
     foreach (Thing* thing, m_evChargers) {
         qCDebug(dcConsolinnoEnergy())
             << "Blackout protection: Checking EV charger thing with name: " << thing->name()
             << " and id: " << thing->id();
 
-        maxAllowedChargingCurrentPhase = thing->thingClass()
-                                             .stateTypes()
-                                             .findByName("maxChargingCurrent")
-                                             .maxValue()
-                                             .toFloat();
-        minAllowedChargingCurrentPhase = thing->thingClass()
-                                             .stateTypes()
-                                             .findByName("maxChargingCurrent")
-                                             .minValue()
-                                             .toFloat();
-        actualChargingCurrentLimitPhase = thing->state("maxChargingCurrent").value().toFloat();
-
-        // qCDebug(dcConsolinnoEnergy())
-        //     << "actual 1: " << thing->state("maxChargingCurrent").value().toFloat();
+        maxChargingCurrentPerPhaseMaxValue = thing->thingClass()
+                                                 .stateTypes()
+                                                 .findByName("maxChargingCurrent")
+                                                 .maxValue()
+                                                 .toFloat();
+        maxChargingCurrentPerPhaseMinValue = thing->thingClass()
+                                                 .stateTypes()
+                                                 .findByName("maxChargingCurrent")
+                                                 .minValue()
+                                                 .toFloat();
+        maxChargingCurrentPerPhase = thing->state("maxChargingCurrentPerPhase").value().toFloat();
 
         qCDebug(dcConsolinnoEnergy())
-            << "Blackout protection: Absolute limits: min=" << minAllowedChargingCurrentPhase
-            << "A, max=" << maxAllowedChargingCurrentPhase
-            << "A, actual value :" << actualChargingCurrentLimitPhase << "A";
+            << "Blackout protection: Absolute limits: min=" << maxChargingCurrentPerPhaseMinValue
+            << "A, max=" << maxChargingCurrentPerPhaseMaxValue
+            << "A, actual value :" << maxChargingCurrentPerPhase << "A";
 
         if (limitExceeded) {
             // If the limit is exceeded, we go down sat least maxPhaseOvershotCurrent
@@ -1163,12 +1196,12 @@ void EnergyEngine::evaluateAndSetMaxChargingCurrent()
                 << maxPhaseOvershotPower * m_housholdPhaseCount
                 << "[W] to much! -> Adjusting the evChargers...";
 
-            float newMaxChargingCurrentLimit = std::max(minAllowedChargingCurrentPhase,
-                actualChargingCurrentLimitPhase - maxPhaseOvershotCurrent - 1);
+            float newMaxChargingCurrentLimit = std::max(maxChargingCurrentPerPhaseMinValue,
+                maxChargingCurrentPerPhase - maxPhaseOvershotCurrent - 1);
 
-            // thing->setStateMaxValue(thing->state("maxChargingCurrent").stateTypeId(),
+            // thing->setStateMaxValue(thing->state("maxChargingCurrentPerPhase").stateTypeId(),
             //     newMaxChargingCurrentLimit); // hier wird nur der max Value gesetzt, nicht der
-            //                                  // maxChargingCurrent
+            //                                  // maxChargingCurrentPerPhase
 
             Action action(ActionTypeId("383854a9-90d8-45aa-bb81-6557400f1a5e"), thing->id());
             ParamList params;
@@ -1181,23 +1214,23 @@ void EnergyEngine::evaluateAndSetMaxChargingCurrent()
                 << "Blackout protection: limitExceeded -> Ajdusted limit of charging current per "
                    "Phase down "
                    "to"
-                << thing->state("maxChargingCurrent").maxValue().toInt() << "A";
+                << thing->state("maxChargingCurrentPerPhase").maxValue().toInt() << "A";
             qCInfo(dcConsolinnoEnergy())
                 << "Blackout protection: limitExceeded -> Ajdusted limit of charging power total "
                    "down "
                    "to"
-                << thing->state("maxChargingCurrent").maxValue().toInt() * 3 * 230 << "W";
+                << thing->state("maxChargingCurrentPerPhase").maxValue().toInt() * 3 * 230 << "W";
         } else {
             // Otherwise we can go up again step by step, only if Margin Power larger than 250W
-            if (actualChargingCurrentLimitPhase < maxAllowedChargingCurrentPhase
+            if (maxChargingCurrentPerPhase < maxChargingCurrentPerPhaseMaxValue
                 && minPhaseMarginPower > 250) {
                 qCDebug(dcConsolinnoEnergy()) << "Blackout protection: lets go up again!";
-                // thing->setStateMaxValue(thing->state("maxChargingCurrent").stateTypeId(),
-                //     std::min(maxAllowedChargingCurrentPhase, actualChargingCurrentLimitPhase +
+                // thing->setStateMaxValue(thing->state("maxChargingCurrentPerPhase").stateTypeId(),
+                //     std::min(maxChargingCurrentPerPhaseMaxValue, maxChargingCurrentPerPhase +
                 //     1));
 
                 float newMaxChargingCurrentLimit
-                    = std::min(maxAllowedChargingCurrentPhase, actualChargingCurrentLimitPhase + 2);
+                    = std::min(maxChargingCurrentPerPhaseMaxValue, maxChargingCurrentPerPhase + 2);
 
                 // Using executeAction to set the new value
                 Action action(ActionTypeId("383854a9-90d8-45aa-bb81-6557400f1a5e"), thing->id());
@@ -1210,43 +1243,14 @@ void EnergyEngine::evaluateAndSetMaxChargingCurrent()
                 qCInfo(dcConsolinnoEnergy())
                     << "Blackout protection: no limitExceeded -> Ajdusted limit of charging "
                        "current up to"
-                    << thing->state("maxChargingCurrent").maxValue().toInt() << "A";
+                    << thing->state("maxChargingCurrentPerPhase").maxValue().toInt() << "A";
             } else {
                 qCDebug(dcConsolinnoEnergy())
-                    << "Blackout protection: maxChargingCurrent not changed because: " << "actual: "
-                    << actualChargingCurrentLimitPhase << " max: " << maxAllowedChargingCurrentPhase
+                    << "Blackout protection: maxChargingCurrentPerPhase not changed because: "
+                    << "actual: " << maxChargingCurrentPerPhase
+                    << " max: " << maxChargingCurrentPerPhaseMaxValue
                     << " minPhaseMarginPower: " << minPhaseMarginPower;
             }
-        }
-    }
-
-    // Adding the logic for the heat pumps
-    foreach (Thing* thing, m_heatPumps) {
-        if (m_consumptionLimit > 0) {
-
-            qCDebug(dcConsolinnoEnergy())
-                << "Blackout protection: Turning off heat pump with name: " << thing->name()
-                << " and id: " << thing->id();
-
-            Action action(ActionTypeId("82b38d32-a277-41bb-a09a-44d6d503fc7a"), thing->id());
-            ParamList params;
-            params.append(Param(ParamTypeId("82b38d32-a277-41bb-a09a-44d6d503fc7a"), "Off"));
-            action.setParams(params);
-            m_thingManager->executeAction(action);
-
-            qCInfo(dcConsolinnoEnergy()) << "Blackout protection: Heat pump turned off.";
-        } else {
-            qCDebug(dcConsolinnoEnergy())
-                << "Blackout protection: Turning on heat pump with name: " << thing->name()
-                << " and id: " << thing->id();
-
-            Action action(ActionTypeId("82b38d32-a277-41bb-a09a-44d6d503fc7a"), thing->id());
-            ParamList params;
-            params.append(Param(ParamTypeId("82b38d32-a277-41bb-a09a-44d6d503fc7a"), "Standard"));
-            action.setParams(params);
-            m_thingManager->executeAction(action);
-
-            qCInfo(dcConsolinnoEnergy()) << "Blackout protection: Heat pump turned on.";
         }
     }
 }
